6991b50b9fa2d180431f6b6a15cd90d96f94556f
Stefano Muraro <66977688+stefanomuraro@users.noreply.github.com>
2026-03-26 23:41:17 -0300

    first commit

---
+++ .config/zsh/custom/aliases.zsh
@@ -0,0 +1,38 @@
+# git
+alias gcane="git commit --amend --no-edit"
+alias gcm="git commit -m"
+alias gsou="git stash --keep-index -u" # only unstaged (incl untracked)
+alias gsos="git stash --staged" # only staged
+alias gsa="git stash -u" # all (staged + unstaged + untracked)
+
+# zsh
+alias zr="edit_zsh $ZDOTDIR/.zshrc"
+alias za="edit_zsh $ZSH_CUSTOM/aliases.zsh"
+alias zf="edit_zsh $ZSH_CUSTOM/functions.zsh"
+alias zs="source $ZDOTDIR/.zshrc"
+
+# overrides
+alias ls="eza -lh --group-directories-first --icons=auto"
+alias lsa="ls -a"
+alias lt="eza --tree --level=2 --long --icons --git"
+alias lta="lt -a"
+alias cd="z"
+alias grep="rg"
+alias find="fd"
+alias cat="bat"
+
+# shortcuts
+alias v="nvim"
+alias t="tmux"
+alias c="clear"
+alias lg="lazygit"
+
+# elixir
+alias rps="iex -S mix phx.server" # run phoenix server
+alias le="mix format && mix credo --strict" # lint elixir
+
+# utils
+alias kp=kill_port
+alias ub=update_branch
+alias cg=clean_gone
+
